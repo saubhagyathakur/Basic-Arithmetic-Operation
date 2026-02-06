c2860ccea2339b6f13ea6666a138abb850b14535
SOUBHAGYA THAKUR <saubhagya325@gmail.com>
2026-02-06 10:22:22 +0530

    Basic-Arithmetic-Operation

---
+++ BASIC-ARITHMETIC.js
@@ -0,0 +1,14 @@
+// Prompt user for two numbers
+let num1 = prompt("Enter the first number:");
+let num2 = prompt("Enter the second number:");
+
+// Convert input strings to numbers
+num1 = Number(num1);
+num2 = Number(num2);
+
+// Perform addition
+let sum = num1 + num2;
+
+// Display result in console
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
